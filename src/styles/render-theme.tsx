90634c24a51df02a6bb9e839800e0db3d716b002
Luiz Otávio <luizomf@gmail.com>
2021-03-01 11:08:48 -0300

    Migrando styles

--- src/styles/render-theme.tsx
+++ src/styles/render-theme.tsx
@@ -1,7 +1,7 @@
-import { render } from '@testing-library/react';
+import { render, RenderResult } from '@testing-library/react';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './theme';
 
-export const renderTheme = (children) => {
+export const renderTheme = (children: React.ReactNode): RenderResult => {
   return render(<ThemeProvider theme={theme}>{children}</ThemeProvider>);
 };
